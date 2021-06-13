210feef36b82607977065aa3f1e290cde59d7c02
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2021-06-12 20:32:47 -0700

    fix brackets

--- lib/hellogoodbyeview.dart
+++ lib/hellogoodbyeview.dart
@@ -125,14 +125,13 @@ class HelloGoodbyeView extends StatelessWidget{
                         );
                         }
                       )
-                    );
-                },
-                child: Image.asset('images/d10.jpg')
+                    ),
+                ],
+                child: Image.asset('images/d10.jpg');
               ),
             ),
-          ]
-      ),
-      ),
+        
+     
     );
   }
 }
