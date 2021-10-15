bd0448a025672168a94eba569b1dc0d13e1e2ff0
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2021-10-14 23:53:42 -0700

    fix import

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'mainscreen.dart';
+import 'hellogoodbyeview.dart';
 
-void main() => runApp(SecCam());
+void main() => runApp(Family());
 
-class SecCam extends StatelessWidget {
-  const SecCam({Key? key}) : super(key: key);
+class Family extends StatelessWidget {
+  const Family({Key? key}) : super(key: key);
 
-  static const String _title = "SecCam";
+  static const String _title = "Family";
 
   Widget build(BuildContext context) {
     return MaterialApp(title: _title, initialRoute: '/', routes: {
