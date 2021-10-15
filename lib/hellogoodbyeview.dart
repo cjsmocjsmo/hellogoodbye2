8d290b0a29976e6b15092811c959820450ba7aa9
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2021-10-14 23:31:09 -0700

    added nav

--- lib/hellogoodbyeview.dart
+++ lib/hellogoodbyeview.dart
@@ -105,141 +105,141 @@ class HelloGoodbyeView extends StatelessWidget{
                 child: Image.asset('images/d10thumb.jpg'),
               )
             ),
-            Container(
-              padding: const EdgeInsets.all(8),
-              child: GestureDetector(
-                onTap: () {
-                  _zoomPage('images/pic1-1.jpg', context);
-                },
-                child: Image.asset('images/pic1-1thumb.jpg'),
-              )
-            ),
-            Container(
-              padding: const EdgeInsets.all(8),
-              child: GestureDetector(
-                onTap: () {
-                  _zoomPage('images/pic2-1.jpg', context);
-                },
-                child: Image.asset('images/pic2-1thumb.jpg'),
-              )
-            ),
-            Container(
-              padding: const EdgeInsets.all(8),
-              child: GestureDetector(
-                onTap: () {
-                  _zoomPage('images/pic3-1.jpg', context);
-                },
-                child: Image.asset('images/pic3-1thumb.jpg'),
-              )
-            ),
-            Container(
-              padding: const EdgeInsets.all(8),
-              child: GestureDetector(
-                onTap: () {
-                  _zoomPage('images/pic4.jpg', context);
-                },
-                child: Image.asset('images/pic4thumb.jpg'),
-              )
-            ),
-            Container(
-              padding: const EdgeInsets.all(8),
-              child: GestureDetector(
-                onTap: () {
-                  _zoomPage('images/pic5.jpg', context);
-                },
-                child: Image.asset('images/pic5thumb.jpg'),
-              )
-            ),
-            Container(
-              padding: const EdgeInsets.all(8),
-              child: GestureDetector(
-                onTap: () {
-                  _zoomPage('images/pic7.jpg', context);
-                },
-                child: Image.asset('images/pic7thumb.jpg'),
-              )
-            ),
-            Container(
-              padding: const EdgeInsets.all(8),
-              child: GestureDetector(
-                onTap: () {
-                  _zoomPage('images/pic8.jpg', context);
-                },
-                child: Image.asset('images/pic8thumb.jpg'),
-              )
-            ),
-            Container(
-              padding: const EdgeInsets.all(8),
-              child: GestureDetector(
-                onTap: () {
-                  _zoomPage('images/pic10.jpg', context);
-                },
-                child: Image.asset('images/pic10thumb.jpg'),
-              )
-            ),
-            Container(
-              padding: const EdgeInsets.all(8),
-              child: GestureDetector(
-                onTap: () {
-                  _zoomPage('images/pic12.jpg', context);
-                },
-                child: Image.asset('images/pic12thumb.jpg'),
-              )
-            ),
-            Container(
-              padding: const EdgeInsets.all(8),
-              child: GestureDetector(
-                onTap: () {
-                  _zoomPage('images/pic9.jpg', context);
-                },
-                child: Image.asset('images/pic9thumb.jpg'),
-              )
-            ),
-            Container(
-              padding: const EdgeInsets.all(8),
-              child: GestureDetector(
-                onTap: () {
-                  _zoomPage('images/p1.jpg', context);
-                },
-                child: Image.asset('images/p1thumb.jpg'),
-              )
-            ),
-            Container(
-              padding: const EdgeInsets.all(8),
-              child: GestureDetector(
-                onTap: () {
-                  _zoomPage('images/p2.jpg', context);
-                },
-                child: Image.asset('images/p2thumb.jpg'),
-              )
-            ),
-            Container(
-              padding: const EdgeInsets.all(8),
-              child: GestureDetector(
-                onTap: () {
-                  _zoomPage('images/p3.jpg', context);
-                },
-                child: Image.asset('images/p3thumb.jpg'),
-              )
-            ),
-            Container(
-              padding: const EdgeInsets.all(8),
-              child: GestureDetector(
-                onTap: () {
-                  _zoomPage('images/p4.jpg', context);
-                },
-                child: Image.asset('images/p4thumb.jpg'),
-              )
-            ),
-            Container(
-              padding: const EdgeInsets.all(8),
-              child: GestureDetector(
-                onTap: () {
-                  _zoomPage('images/p5.jpg', context);
-                },
-                child: Image.asset('images/p5thumb.jpg'),
-              )
-            ),
+            // Container(
+            //   padding: const EdgeInsets.all(8),
+            //   child: GestureDetector(
+            //     onTap: () {
+            //       _zoomPage('images/pic1-1.jpg', context);
+            //     },
+            //     child: Image.asset('images/pic1-1thumb.jpg'),
+            //   )
+            // ),
+            // Container(
+            //   padding: const EdgeInsets.all(8),
+            //   child: GestureDetector(
+            //     onTap: () {
+            //       _zoomPage('images/pic2-1.jpg', context);
+            //     },
+            //     child: Image.asset('images/pic2-1thumb.jpg'),
+            //   )
+            // ),
+            // Container(
+            //   padding: const EdgeInsets.all(8),
+            //   child: GestureDetector(
+            //     onTap: () {
+            //       _zoomPage('images/pic3-1.jpg', context);
+            //     },
+            //     child: Image.asset('images/pic3-1thumb.jpg'),
+            //   )
+            // ),
+            // Container(
+            //   padding: const EdgeInsets.all(8),
+            //   child: GestureDetector(
+            //     onTap: () {
+            //       _zoomPage('images/pic4.jpg', context);
+            //     },
+            //     child: Image.asset('images/pic4thumb.jpg'),
+            //   )
+            // ),
+            // Container(
+            //   padding: const EdgeInsets.all(8),
+            //   child: GestureDetector(
+            //     onTap: () {
+            //       _zoomPage('images/pic5.jpg', context);
+            //     },
+            //     child: Image.asset('images/pic5thumb.jpg'),
+            //   )
+            // ),
+            // Container(
+            //   padding: const EdgeInsets.all(8),
+            //   child: GestureDetector(
+            //     onTap: () {
+            //       _zoomPage('images/pic7.jpg', context);
+            //     },
+            //     child: Image.asset('images/pic7thumb.jpg'),
+            //   )
+            // ),
+            // Container(
+            //   padding: const EdgeInsets.all(8),
+            //   child: GestureDetector(
+            //     onTap: () {
+            //       _zoomPage('images/pic8.jpg', context);
+            //     },
+            //     child: Image.asset('images/pic8thumb.jpg'),
+            //   )
+            // ),
+            // Container(
+            //   padding: const EdgeInsets.all(8),
+            //   child: GestureDetector(
+            //     onTap: () {
+            //       _zoomPage('images/pic10.jpg', context);
+            //     },
+            //     child: Image.asset('images/pic10thumb.jpg'),
+            //   )
+            // ),
+            // Container(
+            //   padding: const EdgeInsets.all(8),
+            //   child: GestureDetector(
+            //     onTap: () {
+            //       _zoomPage('images/pic12.jpg', context);
+            //     },
+            //     child: Image.asset('images/pic12thumb.jpg'),
+            //   )
+            // ),
+            // Container(
+            //   padding: const EdgeInsets.all(8),
+            //   child: GestureDetector(
+            //     onTap: () {
+            //       _zoomPage('images/pic9.jpg', context);
+            //     },
+            //     child: Image.asset('images/pic9thumb.jpg'),
+            //   )
+            // ),
+            // Container(
+            //   padding: const EdgeInsets.all(8),
+            //   child: GestureDetector(
+            //     onTap: () {
+            //       _zoomPage('images/p1.jpg', context);
+            //     },
+            //     child: Image.asset('images/p1thumb.jpg'),
+            //   )
+            // ),
+            // Container(
+            //   padding: const EdgeInsets.all(8),
+            //   child: GestureDetector(
+            //     onTap: () {
+            //       _zoomPage('images/p2.jpg', context);
+            //     },
+            //     child: Image.asset('images/p2thumb.jpg'),
+            //   )
+            // ),
+            // Container(
+            //   padding: const EdgeInsets.all(8),
+            //   child: GestureDetector(
+            //     onTap: () {
+            //       _zoomPage('images/p3.jpg', context);
+            //     },
+            //     child: Image.asset('images/p3thumb.jpg'),
+            //   )
+            // ),
+            // Container(
+            //   padding: const EdgeInsets.all(8),
+            //   child: GestureDetector(
+            //     onTap: () {
+            //       _zoomPage('images/p4.jpg', context);
+            //     },
+            //     child: Image.asset('images/p4thumb.jpg'),
+            //   )
+            // ),
+            // Container(
+            //   padding: const EdgeInsets.all(8),
+            //   child: GestureDetector(
+            //     onTap: () {
+            //       _zoomPage('images/p5.jpg', context);
+            //     },
+            //     child: Image.asset('images/p5thumb.jpg'),
+            //   )
+            // ),
             ],
           ),
         ),
--- lib/mainscreen.dart
+++ lib/mainscreen.dart
@@ -31,6 +31,7 @@ class MainScreen extends StatelessWidget {
                 width: 200.0,
                 child: GestureDetector(
                   onTap: () {
+                    HelloGoodbyeView();
                     // _zoomPage('images/d1.jpg', context);
                   },
                   child: Image.asset("images/d1CThumb.png")
