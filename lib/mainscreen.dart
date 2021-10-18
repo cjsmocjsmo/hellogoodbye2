800dd369a3c8790aac9ea19b4465b64bd7f27ba7
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2021-10-18 06:41:30 -0700

    fix paths

--- lib/mainscreen.dart
+++ lib/mainscreen.dart
@@ -71,7 +71,7 @@ class MainScreen extends StatelessWidget {
                       MaterialPageRoute(builder: (context) => TobbyView()),
                     );
                   },
-                  child: Image.asset("images/circalthub/p4CThumb.png")
+                  child: Image.asset("images/circalthumb/p4CThumb.png")
                 ),
               ),
           
