85f39491ffb0ee19d3c887404c17572a992e5b5f
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2021-10-18 11:19:57 -0700

    update pubspec

--- lib/mainscreen.dart
+++ lib/mainscreen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'cdadview.dart';
 import 'tobbyview.dart';
+import 'texastrip.dart';
 
 // import 'package:http/http.dart' as http;
 // import 'dart:convert';
