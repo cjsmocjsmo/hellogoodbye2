6903e8c3674a17c5c3f1fe498a9c07fe670a1e8e
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2021-10-19 06:37:11 -0700

    added new pic

--- lib/texastrip.dart
+++ lib/texastrip.dart
@@ -46,9 +46,9 @@ class TexasTripView extends StatelessWidget{
               padding: const EdgeInsets.all(8),
               child: GestureDetector(
                 onTap: () {
-                  _zoomPage('images/Tobby/pic3-1.jpg', context);
+                  _zoomPage('images/TexasTrip/20211010/20211010_122551_HDR2.jpg', context);
                 },
-                child: Image.asset('images/thumb/pic3-1thumb.jpg'),
+                child: Image.asset('images/thumb/20211010_122551_HDR_Thumb.jpg'),
               )
             ),
             Container(
