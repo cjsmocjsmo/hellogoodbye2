b9fcfbc625f3f8b1a1b43b3af2f539613e0f1e6b
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2021-10-20 05:56:04 -0700

    added new player

--- lib/youtubeplayer.dart
+++ lib/youtubeplayer.dart
@@ -1,48 +1,59 @@
+import 'package:video_player/video_player.dart';
 import 'package:flutter/material.dart';
-import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
-// void main() {
-//   runApp(MyApp());
-// }
+// void main() => runApp(YouTubePlayerScreen());
 
-// class MyApp extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       // Hide the debug banner
-//       debugShowCheckedModeBanner: false,
+class YouTubePlayerScreen extends StatefulWidget {
+  @override
+  _YouTubePlayerScreenState createState() => _YouTubePlayerScreenState();
+}
 
-//       title: 'Flutter Example',
-//       home: MyHomePage(),
-//     );
-//   }
-// }
+class _YouTubePlayerScreenState extends State<YouTubePlayerScreen> {
+  VideoPlayerController _controller;
 
-class YouTubePlayerScreen extends StatelessWidget {
-  static String myVideoId = 'PQSagzssvUQ';
-  // the full url: https://www.youtube.com/watch?v=PQSagzssvUQ&ab_channel=NASA
-  // it's an interesting video from NASA on Youtube
-
-  // Initiate the Youtube player controller
-  YoutubePlayerController _controller = YoutubePlayerController(
-    initialVideoId: myVideoId,
-    flags: YoutubePlayerFlags(
-      autoPlay: true,
-      mute: false,
-    ),
-  );
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.network(
+        'https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4')
+      ..initialize().then((_) {
+        // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
+        setState(() {});
+      });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text('Flutter and Youtube'),
+    return MaterialApp(
+      title: 'Video Demo',
+      home: Scaffold(
+        body: Center(
+          child: _controller.value.isInitialized
+              ? AspectRatio(
+                  aspectRatio: _controller.value.aspectRatio,
+                  child: VideoPlayer(_controller),
+                )
+              : Container(),
         ),
-        body: Container(
-          child: YoutubePlayer(
-            controller: _controller,
-            liveUIColor: Colors.amber,
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              _controller.value.isPlaying
+                  ? _controller.pause()
+                  : _controller.play();
+            });
+          },
+          child: Icon(
+            _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
           ),
-        ));
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
   }
 }
