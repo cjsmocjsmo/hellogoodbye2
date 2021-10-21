e49db4fd3d852bd3c701e6952f47274caede2d45
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2021-10-20 18:37:37 -0700

    new youtube

---
+++ lib/youtubeplayer2.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: App(),
+    );
+  }
+}
+
+class App extends StatelessWidget {
+  static String videoID = '_b2oRXJPR_A';
+
+  // YouTube Video Full URL : https://www.youtube.com/watch?v=dFKhWe2bBkM&feature=emb_title&ab_channel=BBKiVines
+
+  YoutubePlayerController _controller = YoutubePlayerController(
+    initialVideoId: videoID,
+    flags: YoutubePlayerFlags(
+      autoPlay: false,
+      mute: false,
+    ),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Embed YouTube Video in Flutter'),
+        ),
+        body: Container(
+          child: YoutubePlayer(
+            controller: _controller,
+            liveUIColor: Colors.amber,
+            showVideoProgressIndicator: true,
+          ),
+        ));
+  }
+}
