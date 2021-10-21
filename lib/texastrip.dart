3c76f703af5b2675697bfbef228247925254254f
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2021-10-21 10:02:40 -0700

    test

--- lib/texastrip.dart
+++ lib/texastrip.dart
@@ -313,7 +313,7 @@ class TexasTripView extends StatelessWidget{
         ),
         child: GridView.builder(
           gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-            maxCrossAxisExtent: 200,
+            maxCrossAxisExtent: 100,
             childAspectRatio: 3 / 2,
             crossAxisSpacing: 20,
             mainAxisSpacing: 20),
